96f170877ea472bdf72754e375d93c0841aa567b
Adrian Di Tosto <amditosto@gmail.com>
2023-07-24 14:56:46 -0400

    removed component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import Table from "./components/Table";
 import HeaderBanner from "./components/headerBanner";
-import GetDateTime from "./components/getDateTime";
 
 export default function App() {
+  
   return (
     <div>
-      <HeaderBanner titleText={"Header"} />
+      <HeaderBanner titleText={"SuperMarket FIFO Report"} />
       <Table />
-      <GetDateTime />
     </div>
   );
 }
