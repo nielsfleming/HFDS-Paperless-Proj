8b0a173b7095ecd0ce08055fa2132640fce5a9df
nielsfleming <nielspfleming143@gmail.com>
2023-07-25 10:00:00 -0400

    changed folder heirarchy

--- hfds-react-frontend/src/App.tsx
+++ hfds-react-frontend/src/App.tsx
@@ -3,11 +3,17 @@ import Table from "./components/Table";
 import HeaderBanner from "./components/headerBanner";
 
 export default function App() {
-  
+  const data = {
+    partNumber: 123456,
+    warehouse: "warehouse#1",
+    bins: 12,
+    quantity: 600,
+  };
+
   return (
     <div>
       <HeaderBanner titleText={"SuperMarket FIFO Report"} />
-      <Table />
+      <Table {...data} />
     </div>
   );
 }
