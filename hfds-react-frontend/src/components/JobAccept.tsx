0daa3a3a934ac9f4a34459c5681373481f069e71
nielsfleming <nielspfleming143@gmail.com>
2023-08-01 14:01:47 -0400

    Added new Prettier files.

---
+++ hfds-react-frontend/src/components/JobAccept.tsx
@@ -0,0 +1,13 @@
+import { Modal, Button } from "fecomponents";
+import { useState } from "react";
+
+export default function JobAccept() {
+    
+  return (
+    <div>
+      <Modal>
+        <Button type="success">Accept</Button>
+      </Modal>
+    </div>
+  );
+}
