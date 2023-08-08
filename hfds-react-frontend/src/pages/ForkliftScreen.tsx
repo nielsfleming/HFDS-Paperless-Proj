f2299c8c18e02452f480bfe9217ccb6a51bacf2b
nielsfleming <nielspfleming143@gmail.com>
2023-08-08 08:30:06 -0400

    Reoraganized screen display. Stopping development for term

---
+++ hfds-react-frontend/src/pages/ForkliftScreen.tsx
@@ -0,0 +1,55 @@
+import Table, { TableProps } from '../components/Table';
+import JobAccept from '../components/JobAccept';
+import { Navbar } from 'fecomponents';
+import { useState, useEffect } from 'react';
+import logo from '../img/martinreaLogo.png';
+
+export default function ForkliftScreen({}) {
+    const [data, setData] = useState([{}]);
+    const [open, setOpen] = useState(true);
+    const [scrub, setScrub] = useState({label: 'scrub', description: 'scrub-a-dub-dub'});
+
+    useEffect(() => {
+        fetch('http://localhost:5000/members')
+            .then(res => res.json())
+            .then(data => {
+                setData(data);
+                console.log('in data print');
+                console.log(data);
+            });
+    }, []);
+
+    const handleModalOpen = () => {
+        setOpen(true);
+    }
+
+    // data will be pased in as table properties, received via http request
+    const props: TableProps & React.Dispatch<React.SetStateAction<boolean>> & { setScrub: React.Dispatch<React.SetStateAction<{ label: string; description: string }>> } = {
+        item: [
+            {
+                partNumber: 123456,
+                warehouse: 'Warehouse 1',
+                bins: 12,
+                quantity: 600,
+                fifoLocation: 'G245'
+            },
+            {
+                partNumber: 654321,
+                warehouse: 'Warehouse 2',
+                bins: 8,
+                quantity: 400,
+                fifoLocation: 'G420'
+            }
+        ],
+        setScrub: setScrub,
+        setOpen: setOpen,
+    };
+
+    return (
+        <div>
+            <Navbar icon={<img src={logo} width={50} height={50}/>} pageTitle='Supermarket FIFO Report' variant='display' />
+            <Table {...props} setScrub={setScrub} setOpen={handleModalOpen} />
+            <JobAccept open={open} setOpen={setOpen} scrub={scrub} />
+        </div>
+    );
+}
